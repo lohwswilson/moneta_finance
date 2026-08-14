2765b3ab386d2f848e770c64404462e3a8675c47
WS Loh <wsloh@perfectwork.com>
2026-08-14 19:38:03 +0800

    feat: custom account sequencing and drag-and-drop handle reordering

--- models/account.py
+++ models/account.py
@@ -8,8 +8,9 @@ from odoo.exceptions import ValidationError
 class MonetaAccount(models.Model):
     _name = 'moneta.account'
     _description = 'Moneta Account'
-    _order = 'is_favourite desc, name'
+    _order = 'sequence, is_favourite desc, name'
 
+    sequence = fields.Integer(string='Sequence', default=10, index=True)
     name = fields.Char(string='Account Name', required=True)
     account_type = fields.Selection([
         ('checking', 'Checking'),
